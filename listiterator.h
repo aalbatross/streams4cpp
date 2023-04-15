144e7d1bfbe2353d5922037b3771846c53dceba2
ravipathak <iamrp22590>
2023-04-15 13:37:27 +0100

    updating list iterator

--- listiterator.h
+++ listiterator.h
@@ -5,34 +5,39 @@
 
 namespace aalbatross::util {
 
-    template<typename Iter,
-             typename T = typename std::iterator_traits<Iter>::value_type>
-    struct ListIterator : public Iterator<T> {
-        ListIterator(Iter &&begin, Iter &&end)
-            : d_begin(begin), d_end(end), d_current(begin), d_last(*begin) {}
+template<typename Iter,
+         typename T = typename std::iterator_traits<Iter>::value_type>
+struct ListIterator : public Iterator<T> {
+  ListIterator(Iter &&begin, Iter &&end)
+      : d_begin(begin), d_end(end), d_current(begin), d_last(*begin) {}
 
-        ~ListIterator() = default;
+  ~ListIterator() = default;
 
-        inline bool hasNext() override { return d_current != d_end; }
+  inline bool hasNext() override {
+    bool hasMore = d_current != d_end;
+    if (hasMore) {
+      d_last = *d_current;
+      std::advance(d_current, 1);
+    }
+    return hasMore;
+  }
 
-        inline T next() override {
-            d_last = *d_current;
-            std::advance(d_current, 1);
-            return d_last;
-        }
+  inline T next() override {
+    return d_last;
+  }
 
-        inline void reset() override { d_current = d_begin; }
+  inline void reset() override { d_current = d_begin; }
 
-    private:
-        Iter d_begin;
-        Iter d_end;
-        Iter d_current;
-        T d_last;
-    };
+ private:
+  Iter d_begin;
+  Iter d_end;
+  Iter d_current;
+  T d_last;
+};
 
-    template<typename Iter,
-             typename T = typename std::iterator_traits<Iter>::value_type>
-    ListIterator(Iter &&, Iter &&) -> ListIterator<Iter, T>;
+template<typename Iter,
+         typename T = typename std::iterator_traits<Iter>::value_type>
+ListIterator(Iter &&, Iter &&) -> ListIterator<Iter, T>;
 
 };// namespace aalbatross::util
 
