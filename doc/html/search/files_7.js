72ddedc20d53190a9cdbf1117d21a6fdd8ce0615
ravipathak <iamrp22590>
2023-05-13 22:20:25 +0100

    updating benchmark and docs

---
+++ doc/html/search/files_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ub_5fstream_2eh_0',['ub_stream.h',['../ub__stream_8h.html',1,'']]],
+  ['userguide_2emd_1',['UserGuide.md',['../_user_guide_8md.html',1,'']]],
+  ['utils_2eh_2',['utils.h',['../utils_8h.html',1,'']]]
+];
---
+++ doc/html/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['user_20guide_0',['User Guide',['../md_doc__user_guide.html',1,'']]]
+];
